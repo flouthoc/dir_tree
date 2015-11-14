947b67435dc014199cf2ad1ed484746f3d678367
flouthoc <flouthoc@users.noreply.github.com>
2015-11-14 20:37:20 +0530

    Create dir_tree.py

---
+++ dir_tree.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#_author_ = flouthoc (http://github.com/flouthoc)
+import os
+string = ""
+def rec_tree(path, string, append):
+	CYAN='\033[94m'
+	WHITE='\033[1;37m'
+	f=""
+	point1 = "──"
+	point = append + '├' + point1
+	files = [f for f in os.listdir(path) if os.path.isfile(path+'/'+f)]
+	
+	if(append == ""):
+		append = '   '
+
+	append += '   '
+
+	for x in files:
+		print "%s%s" %(point, x)
+
+	directory = [f for f in os.listdir(path) if os.path.isdir(path+'/'+f)]
+	for x in directory:
+		point = point.replace('├', '└')
+		print "%s%s%s%s" %(point, CYAN, x, WHITE)
+		rec_tree(path+'/'+x, string, append)
+	
+
+rec_tree('.', string, "")
+	
